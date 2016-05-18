270d59fa7a9276ec6472285b13236e48b10ecf9e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-18 21:53:08 +0200

    Create a grid of cells
    The grid is responsible for maintaining the cells that play a part

---
+++ solver/src/grid/mod.rs
@@ -0,0 +1,40 @@
+use std::ops::Index;
+use super::cell::Cell;
+
+pub struct Grid {
+    cells: Vec<Cell>,
+}
+
+impl Grid {
+    pub fn new() -> Grid {
+        let mut cells = vec!();
+        for _ in 0..81 {
+            cells.push(Cell::new());
+        }
+        Grid {
+            cells: cells,
+        }
+    }
+}
+
+impl Index<(i32,i32)> for Grid {
+    type Output = Cell;
+
+    fn index<'a>(&'a self, tuple: (i32, i32)) -> &'a Cell {
+        &self.cells[(9 * tuple.0 + tuple.1) as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn should_index_cells() {
+        let grid: Grid = Grid::new();
+
+        let ref cell = grid[(8, 8)];
+
+        assert_eq!(vec!(1, 2, 3, 4, 5, 6, 7, 8, 9), cell.options());
+    }
+}
