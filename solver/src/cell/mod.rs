0d75ebb3caac37f83cc8dc33242f4b0378b105d4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-18 21:24:25 +0200

    Assign a choice to Cell

--- solver/src/cell/mod.rs
+++ solver/src/cell/mod.rs
@@ -1,20 +1,40 @@
+enum Binding {
+    Unbound,
+    Bound(i32),
+}
+
 pub struct Cell {
-    unavailable_options: Vec<i32>
+    unavailable_options: Vec<i32>,
+    binding: Binding,
 }
 
 impl Cell {
     pub fn new() -> Cell {
-        Cell { unavailable_options: vec!() }
+        Cell {
+            unavailable_options: vec!(),
+            binding: Binding::Unbound,
+        }
     }
 
     pub fn options(&self) -> Vec<i32> {
-        let mut options: Vec<i32> = vec!(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        options.retain(|candidate: &i32|{ !self.unavailable_options.contains(candidate) });
-        options
+        match self.binding {
+            Binding::Unbound  => {
+                let mut options: Vec<i32> = vec!(1, 2, 3, 4, 5, 6, 7, 8, 9);
+                options.retain(|candidate: &i32|{ !self.unavailable_options.contains(candidate) });
+                options
+            }
+            Binding::Bound(_) => {
+                vec!()
+            }
+        }
     }
 
     pub fn restrict(&mut self, option: i32) {
         self.unavailable_options.push(option);
+    }
+
+    pub fn assign(&mut self, choice: i32) {
+        self.binding = Binding::Bound(choice);
     }
 }
 
@@ -47,5 +67,15 @@ mod tests {
         cell.restrict(6);
 
         assert_eq!(vec!(1,2,3,4,5,8), cell.options());
+    }
+
+    #[test]
+    fn should_have_no_options_when_assigned() {
+        let mut cell: Cell = Cell::new();
+        let no_options: Vec<i32> = vec!();
+
+        cell.assign(1);
+
+        assert_eq!(no_options, cell.options());
     }
 }
